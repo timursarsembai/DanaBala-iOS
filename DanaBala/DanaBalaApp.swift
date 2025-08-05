533b16115a58681f22da532772dcdecc7444b65f
Timur Sarsembai <sarsembai.timur@gmail.com>
2025-08-05 23:52:07 +0500

    Initial Commit

---
+++ DanaBala/DanaBalaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DanaBalaApp.swift
+//  DanaBala
+//
+//  Created by Timur Sarsembai on 11/02/1447 AH.
+//
+
+import SwiftUI
+
+@main
+struct DanaBalaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DanaBalaTests/DanaBalaTests.swift
@@ -0,0 +1,17 @@
+//
+//  DanaBalaTests.swift
+//  DanaBalaTests
+//
+//  Created by Timur Sarsembai on 11/02/1447 AH.
+//
+
+import Testing
+@testable import DanaBala
+
+struct DanaBalaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
